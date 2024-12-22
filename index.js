051837e1272db1ee0ed342a1a70dfd2f8ee142ef
Memby Poudel <officialmembypoudel@gmail.com>
2024-12-22 14:09:38 +0545

    c7 progress ws 8

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import http from 'http';
 import express from 'express';
 import { WebSocket, WebSocketServer } from 'ws';
+import { state } from './model/state.js';
 
 const app = express();
 
@@ -15,7 +16,35 @@ const wss = new WebSocketServer({ server });
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     console.log(`Received message => ${message}`);
-    ws.send(`Welcome: ${message}`);
+
+    try {
+      const data = JSON.parse(message);
+      console.log(data);
+      if (data.message === 'fetch') {
+        ws.send(JSON.stringify(state[0]));
+      } else if (data.message === 'update') {
+        state[0] = {
+          name: data.name,
+          state: data.state,
+        };
+        wss.clients.forEach((client) => {
+          client.send(JSON.stringify(state[0]));
+        });
+      } else {
+        ws.send(
+          JSON.stringify({
+            message: 'Unknown Command',
+          })
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+      ws.send(
+        JSON.stringify({
+          message: error.message,
+        })
+      );
+    }
   });
 });
 
