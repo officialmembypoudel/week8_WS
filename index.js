53fadcec6abda92351acf87e0e3210f1484c812e
Memby Poudel <officialmembypoudel@gmail.com>
2024-12-22 12:02:38 +0545

    c9 progress

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import express from 'express';
 import http from 'http';
+import { WebSocketServer } from 'ws';
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Server is Working' });
+  res.json({ message: 'Server Is working' });
 });
 
 const server = http.createServer(app);
 
+const wss = new WebSocketServer({ server: server });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log(`Received Message = ${message}`);
+    ws.send(`Server Received Message = ${message}`);
+  });
+});
+
 server.listen(443, () => {
-  console.log('Server is listening on Port 443');
+  console.log('Server is Running on port 443');
 });
