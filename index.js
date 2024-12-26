935f674e8edb86895ab5a4604c9565135221fd0d
Memby Poudel <officialmembypoudel@gmail.com>
2024-12-26 12:28:23 +0545

    c9 progress week8 last

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import http from 'http';
 import { WebSocketServer } from 'ws';
 
+import { state } from './model/state.js';
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -15,7 +17,38 @@ const wss = new WebSocketServer({ server: server });
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     console.log(`Received Message = ${message}`);
-    ws.send(`Server Received Message = ${message}`);
+
+    try {
+      const data = JSON.parse(message);
+
+      if (data.message === 'fetch') {
+        const ledState = state[0];
+
+        ws.send(JSON.stringify(ledState));
+      } else if (data.message === 'update') {
+        state[0] = {
+          name: 'Led',
+          state: data.state,
+        };
+
+        wss.clients.forEach((client) => {
+          client.send(JSON.stringify(state[0]));
+        });
+      } else {
+        ws.send(
+          JSON.stringify({
+            message: ' command Not Found',
+          })
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+      ws.send(
+        JSON.stringify({
+          message: error.message,
+        })
+      );
+    }
   });
 });
 
